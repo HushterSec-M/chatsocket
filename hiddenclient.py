f48fcd3993e418fb4839b844754fbaa0cd411410
Cavermore <44120986+HushterSec-M@users.noreply.github.com>
2019-11-10 16:47:09 +0300

    Delete hiddenclient.py

--- hiddenclient.py
+++
@@ -1,7 +0,0 @@
-import socket
-server = '192.168.43.3', 5000
-sok = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sok.bind(('', 0))
-while 1:
-    data = sok.recv(1024)
-    print(data.decode('utf-8'))
